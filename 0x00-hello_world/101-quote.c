bb33b4f1808a8e46b91a9822cb786880cc0764e6
free-care <110991027+free-care@users.noreply.github.com>
2022-09-23 22:18:29 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+*  main - Entry point
+*
+*  Return - Always (Success)
+*/
+
+int main (void)
+{
+write ("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n);
+return (1);
+}
