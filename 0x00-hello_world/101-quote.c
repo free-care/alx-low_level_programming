ad59c18bed598cef95654efa4d98a50b10dc71fa
free-care <110991027+free-care@users.noreply.github.com>
2022-09-23 22:39:29 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
 *  main - Entry point
 *
-*  Return - Always 1 (Success)
+*  Return: Always (Success)
 */
 
 int main(void)
 {
-putc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
