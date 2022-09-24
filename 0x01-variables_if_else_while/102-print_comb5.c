2c391ef7583b22dbde6099d644e7ffe83135af54
free-care <110991027+free-care@users.noreply.github.com>
2022-09-25 00:55:08 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,35 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*  main - print digit nos of base 16
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+    int n, i, j, k;
+
+    for (n = '0'; n <= '9'; n++)
+    {
+        for (i = '0'; i <= '8'; i++)
+        {
+             for (j = '0'; j <= '9'; j++)
+             {
+                 for (k = '0'; k <= '9'; k++)
+                 {
+                     if ((n == i || n > i) < (j < k || j >= k))
+                     {
+                         putchar(n);
+                         putchar(i);
+                         putchar(' ');
+                         putchar(j);
+                         putchar(k);
+                     }
+                 }
+             }
+        }
+    }
+    putchar('\n');
+    return (0);
+}
