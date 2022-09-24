91de091aae960c210aecaaed1b975b9a67aa052d
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 19:34:56 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*  main - putchar tolower case alphabet
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+char c;
+
+for (c = 'Z'; c >= 'A'; c--)
+putchar(tolower(c));
+
+putchar('\n');
+return (0);
+}
