9f89b629b93c146e55a85f6124243489c8994f1b
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 19:40:45 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*  main - print digit nos of base 16
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '15'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
