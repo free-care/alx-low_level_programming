5e574705963d02cdf7e73e4b6a7028eab8aef9a5
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 13:44:34 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,22 @@
-#!/bin/bash
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*  main - print digit nos
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+int i;
+
+i = 0;
+while (1)
+{
+puts(i);
+i++;
+}
+puts("\n")
+return (0);
+}
