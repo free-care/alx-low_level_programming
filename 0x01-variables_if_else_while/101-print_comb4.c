6a9bda80e8e87872f5762e2060c455024300883c
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 23:45:34 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,36 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*  main - prints all possible different combinations of three digits
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+int n, i, j;
+
+for (n = '0'; n <= '7'; n++)
+{
+for (i = '1'; i <= '8'; i++)
+{
+for (j = '2'; j <= '9'; j++)
+if (n != i && i != j || n > i && i > j)
+{
+if (n < i && i < j)
+{
+putchar(n);
+putchar(i);
+putchar(j);
+if (n != '7' || i != '8' || j != '9')
+{
+ putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
