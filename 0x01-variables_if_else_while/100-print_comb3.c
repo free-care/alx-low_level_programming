47990051c59e01ae599073aeb16695ce2a3cd4f7
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 23:08:14 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,27 @@
 
 int main(void)
 {
-    int n, i;
+int n, i;
 
-    for (n = '0'; n < '9'; n++)
-    {
-        for (i = '1'; i <= '9'; i++)
-        {
-            if (n != i || n > i)
-            {
-                if (n < i)
-                {
-                    putchar(n);
-                    putchar(i);
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+for (n = '0'; n < '9'; n++)
+{
+for (i = '1'; i <= '9'; i++)
+{
+if (n != i || n > i)
+{
+ if (n < i)
+{
+putchar(n);
+putchar(i);
+if (n != '8' || i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
