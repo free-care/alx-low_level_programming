56bbd8cda879cd96abf88b7306429cb058bee378
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 13:28:41 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,35 @@
-#!/bin/bash
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*  main - putchar tolower case alphabet
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+char c, d, e;
+
+c = 'A';
+while (c < 'E')
+{
+putchar(tolower(c));
+c++;
+}
+d = 'F';
+while (d < 'Q')
+{
+putchar(tolower(d));
+d++;
+}
+e = 'R';
+while (e <= 'Z')
+{
+putchar(tolower(e));
+e++;
+}
+
+putchar('\n');
+return (0);
+}
