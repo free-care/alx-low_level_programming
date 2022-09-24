e1a5509e1a1a275e9f351933756fafd89f214f11
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 20:06:10 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,41 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*  main - print digit nos of base 10
+*
+*  Return: Always (Success)
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*  main - putchar tolower case alphabet
+*
+*  Return: Always (Success)
+*/
+
+#include <stdio.h>
+
+/**
+*  main - print digit nos of base 16
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+int n, i;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+if (n < '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
