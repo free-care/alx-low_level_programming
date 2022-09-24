0e73f793655c4c131decf0ffabb3c8f26078ae62
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 16:17:23 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*  main - print digit nos of base 10
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+char c;
+
+c = '0';
+while(c % '10')
+{
+    putchar(c);
+    c++;
+}
+putchar("\n");
+return (0);
+}
