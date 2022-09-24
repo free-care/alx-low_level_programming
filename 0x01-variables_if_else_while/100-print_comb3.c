568e98e1e9c987f6bf2007bc9aa239baa10a8450
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 21:02:40 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,29 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*  main - print digit nos of base 16
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+    int n, i;
+
+    for (n = '0'; n < '9'; n++)
+    {
+        for (i = '1'; i <= '9'; i++)
+        {
+            if (n != i || n > i)
+            {
+                if (n < i)
+                {
+                    putchar(n);
+                    putchar(i);
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+}
