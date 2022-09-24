4c6472c26e4ddc3c2a22b72a3b0a512e29fd3e60
free-care <110991027+free-care@users.noreply.github.com>
2022-09-24 13:04:11 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*  main - putchar tolower case alphabet
+*
+*  Return: Always (Success)
+*/
+
+int main(void)
+{
+char c;
+
+for (c = 'A'; c <= 'Z'; c++)
+putchar(tolower(c));
+putchar(c);
+
+putchar('\n');
+return (0);
+}
